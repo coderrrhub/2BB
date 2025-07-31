4db69ce4c16f09fbf65b6fc992091d8a36dfc241
only-dsa <itsjannesar@gmail.com>
2025-07-31 17:31:52 +0530

    Lectures till User Input

---
+++ Lecture1/Practice.java
@@ -0,0 +1,18 @@
+package Lecture1;
+
+public class Practice {
+	public static void main(String[] args) {
+//		System.out.println("Hello World"); // print karega then next line pe jayega
+//		System.out.print("Bye"); // only print karega
+//		System.out.println("Kaise Ho?");
+		
+//		int sum;
+//		sum = 10;
+		byte b = 128;
+		System.out.println(b);
+		int sum = 10;
+		System.out.println(sum); // sum = 10
+		sum = 20;
+		System.out.println(sum); // sum = 20
+	}
+}
---
+++ Lecture2/Practice.java
@@ -0,0 +1,24 @@
+package Lecture2;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		short b = 32768;
+//		long n = 21474836478l;
+//		float n = 127.70f;
+		// short [-32768, 32767]
+		int n = 97; //[-2^31, 2^31-1]
+		char ch = (char)n; // 'a' ---> 97
+		System.out.println(ch);
+		byte b = (byte)128.50;
+//		System.out.println(Integer.MAX_VALUE);
+//		int n = (int)127.50;
+//		short s = (short)32768;
+//		System.out.println(n);
+//		byte b =(byte) -130; // explicit type conversion
+//		System.out.println(b);
+//		System.out.println(b);
+	}
+
+}
