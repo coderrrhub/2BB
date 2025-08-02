ec2a4336aa6d7fcb4fe353babb0f08e86faf26ab
only-dsa <itsjannesar@gmail.com>
2025-08-02 12:24:05 +0530

    matching the folder name with the oneNote page link

---
+++ Lecture4/Practice.java
@@ -0,0 +1,14 @@
+package Lecture3;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter = 1;
+		while(counter <= 5) {
+			System.out.print("*");
+			counter++;
+		}
+	}
+
+}
